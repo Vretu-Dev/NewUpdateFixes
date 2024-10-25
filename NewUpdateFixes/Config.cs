e1abc7311fcda4442db90de9dfcdde7496080e6f
Vretu-Dev <jakub.spieczny1@gmail.com>
2024-10-25 13:53:55 +0200

    Code cleanup + jailbird support

--- NewUpdateFixes/Config.cs
+++ NewUpdateFixes/Config.cs
@@ -1,31 +1,25 @@
-﻿using Exiled.API.Interfaces;
-using System;
-using System.Collections.Generic;
+﻿using Exiled.API.Enums;
+using Exiled.API.Interfaces;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 
 namespace NewUpdateFixes
 {
     public class Config : IConfig
     {
-
-        [Description("Whether or not this plugin is enabled. ")]
+        [Description("Whether or not this plugin is enabled.")]
         public bool IsEnabled { get; set; } = true;
-
         [Description("Whether or not debug messages should be shown in the console.")]
         public bool Debug { get; set; } = false;
-
         [Description("revert scp207 speeds back to its pre 13.5 speeds")]
         public bool OldColaSpeed { get; set; } = true;
-
         [Description("revert scp207 health drain to its pre 13.5 values")]
         public bool OldColaHealthDrain { get; set; } = true;
-
         [Description("If SCP500 cures the traumatised affect given by 106. - suggested by follow the owl")]
         public bool Scp500CuresTrauma { get; set; } = false;
-
+        [Description("Jailbird Custom Settings:")]
+        public bool EnableCustomJailbirdSettings { get; set; } = false;
+        public EffectType JailbirdEffect { get; set; } = EffectType.Flashed;
+        public float JailbirdEffectDuration { get; set; } = 4f;
+        public byte JailbirdEffectIntensity { get; set; } = 1;
     }
 }
