742a64d78a4302f2312791680690ea075b96c4b1
Vretu-Dev <jakub.spieczny1@gmail.com>
2024-11-11 14:37:34 +0100

    Update: Exiled Version, Readme.md and config

--- NewUpdateFixes/Config.cs
+++ NewUpdateFixes/Config.cs
@@ -10,11 +10,11 @@ namespace NewUpdateFixes
         public bool IsEnabled { get; set; } = true;
         [Description("Whether or not debug messages should be shown in the console.")]
         public bool Debug { get; set; } = false;
-        [Description("revert scp207 speeds back to its pre 13.5 speeds")]
+        [Description("Revert SCP-207 speeds back to its pre 13.5 speeds.")]
         public bool OldColaSpeed { get; set; } = true;
-        [Description("revert scp207 health drain to its pre 13.5 values")]
+        [Description("Revert SCP-207 health drain to its pre 13.5 values.")]
         public bool OldColaHealthDrain { get; set; } = true;
-        [Description("If SCP500 cures the traumatised affect given by 106. - suggested by follow the owl")]
+        [Description("If SCP-500 cures the traumatised effect caused by SCP-106 - suggested by follow The Owl.")]
         public bool Scp500CuresTrauma { get; set; } = false;
         [Description("Jailbird Custom Settings.")]
         public bool EnableCustomJailbirdSettings { get; set; } = false;
