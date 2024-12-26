b00b36b02e44d5373cb001f32e0456d2ceda91b7
Vretu-Dev <jakub.spieczny1@gmail.com>
2024-12-27 00:30:03 +0100

    Exiled 9.1.0

--- NewUpdateFixes/NewUpdateFixes.cs
+++ NewUpdateFixes/NewUpdateFixes.cs
@@ -8,7 +8,7 @@ namespace NewUpdateFixes
         public override string Name => "NewUpdateFixes";
         public override string Author => "Half";
         public override string Prefix { get; } = "NUF";
-        public override Version Version => new Version(1, 3, 1);
+        public override Version Version => new Version(1, 3, 2);
         public override Version RequiredExiledVersion { get; } = new Version(9, 0, 0);
         public static NewUpdateFixes Instance { get; private set; }
         public override void OnEnabled()
